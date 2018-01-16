68fe69fb73670edc403124b256d189ad41e74008
Aldwin Vlasblom <aldwin.vlasblom@gmail.com>
2018-01-16 11:21:21 +0100

    Use the appropriate global scope
    Fixes #204

--- src/internal/bc.js
+++ src/internal/bc.js
@@ -1,1 +1,12 @@
-export function setImmediate(f, x){ return setTimeout(f, 0, x) }
+/* istanbul ignore file: environment-specific */
+
+/* eslint-disable no-undef */
+export const scope = typeof self === 'object' ? self :
+                     typeof global === 'object' ? global :
+                     typeof window === 'object' ? window :
+                     this;
+/* eslint-enable no-undef */
+
+export const setImmediate = typeof scope.setImmediate === 'function' ?
+                            scope.setImmediate :
+                            function setImmediate(f, x){ return setTimeout(f, 0, x) };
