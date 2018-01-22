b9079b1bb4b3d6a5dc715f542612998e5e480467
Aldwin Vlasblom <aldwin.vlasblom@gmail.com>
2018-01-22 11:31:51 +0100

    Remove a test with a race condition

--- test/0.bc.test.js
+++ test/0.bc.test.js
@@ -1,4 +1,3 @@
-import {expect} from 'chai';
 import * as bc from '../src/internal/bc';
 
 describe('bc', function(){
@@ -7,15 +6,6 @@ describe('bc', function(){
 
     it('calls the given function with the given argument', function(done){
       bc.setImmediate(done, null);
-    });
-
-    it('schedules the function call relatively quickly', function(done){
-      var message = '';
-      bc.setImmediate(function(x){ message = x }, 'hello');
-      setTimeout(function(){
-        expect(message).to.equal('hello');
-        done();
-      });
     });
 
   });
