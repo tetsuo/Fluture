01b4d4b2576fafcf39469815e129eb246d50d368
Aldwin Vlasblom <aldwin@avaq.it>
2019-05-08 17:23:12 +0200

    Remove some dead code left over from the refactor

--- src/promise.mjs
+++ src/promise.mjs
@@ -1,8 +1,8 @@
 import {application1, future} from './internal/check';
 
 export function promise(m){
-  var context1 = application1(promise, future, m);
+  application1(promise, future, m);
   return new Promise(function promise$computation(res, rej){
-    m._interpret(rej, rej, res, context1);
+    m._interpret(rej, rej, res);
   });
 }
